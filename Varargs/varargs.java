834b27d44c69baaac6d653c23a531767e4b42f12
Sameer Talekar <sameertalekar2007@gmail.com>
2026-06-27 22:51:19 +0530

    Changed some code

--- Varargs/varargs.java
+++ Varargs/varargs.java
@@ -2,7 +2,8 @@
 public class varargs {
 
     public static void main(String[] args) {
-        System.out.print(add(2, 4, 5, 7, 9));
+        System.out.println(add(2, 4, 5, 7, 9));
+        System.out.println(average(1, 3, 5, 7, 9));
     }
 
     static int add(int... numbers) {
@@ -11,5 +12,13 @@ public class varargs {
             sum += number;
         }
         return sum;
+    }
+
+    static double average(double... numbers) {
+        double sum = 0;
+        for (double number : numbers) {
+            sum += number;
+        }
+        return sum / numbers.length;
     }
 }
